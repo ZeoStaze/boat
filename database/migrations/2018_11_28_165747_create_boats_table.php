4fc6f0b9a7613091aee1497f26f614d9e97480ed
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-05 16:46:54 +0100

    Initial commit

---
+++ database/migrations/2018_11_28_165747_create_boats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nom', 25);
+            $table->string('modele');
+            $table->decimal('longueur', 4,2);
+            $table->decimal('largeur', 4,2);
+            $table->date('date_construction');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boats');
+    }
+}
